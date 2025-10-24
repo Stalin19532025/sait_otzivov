f28ec34f95481ad5ba84e49d336c577f5d2a8856
Саня <zhuchkova.alex-06@mail.ru>
2025-10-24 19:38:46 +0300

    а

--- django/Паша есть поезда на ужин и потом бежит на туалет/ядро/settings.py
+++ django/Паша есть поезда на ужин и потом бежит на туалет/ядро/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "html")]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "медиа")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
