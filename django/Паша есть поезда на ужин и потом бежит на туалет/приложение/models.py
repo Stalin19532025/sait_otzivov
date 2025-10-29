48ae76d91e7f92eeaf165af04676afad2c36b90e
Саня <zhuchkova.alex-06@mail.ru>
2025-10-29 19:40:00 +0300

    1

--- django/Паша есть поезда на ужин и потом бежит на туалет/приложение/models.py
+++ django/Паша есть поезда на ужин и потом бежит на туалет/приложение/models.py
@@ -10,6 +10,7 @@ from django.contrib.auth import get_user_model
 
 # каждая модель (каждый класс) наследуются от стандартного класс джанго(принимает его стандартные свойства). 
 class Отзыв(models.Model):
+    автор = models.ForeignKey(Юзер, on_delete=models.CASCADE)
     # каждая переменная класса - это название поля(колонки и таблицы)
     дата_поста = models.DateTimeField(
         # это поле автоматически считывает время компьютера и присваевает его тв качестве6 значения в момент создания записи в БД
@@ -21,3 +22,12 @@ class Отзыв(models.Model):
     )
 
     текст = models.TextField()
+
+    class Meta:
+            verbose_name = "Отзыв"
+            verbose_name_plural = "Отзывы"
+
+    def __str__(self):
+          if len(self.текст) > 50:
+                return self.текст[:50] + "..."
+          return self.текст
