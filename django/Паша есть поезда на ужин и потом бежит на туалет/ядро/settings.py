7d46c14f1110ba18413f771668e80e0df88493f9
Саня <zhuchkova.alex-06@mail.ru>
2025-11-19 19:39:50 +0300

    d

--- django/Паша есть поезда на ужин и потом бежит на туалет/ядро/settings.py
+++ django/Паша есть поезда на ужин и потом бежит на туалет/ядро/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'приложение.apps.ПриложениеConfig',
+    'users.apps.UsersConfig',
+    "widget_tweaks",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "медиа")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
